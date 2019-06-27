0c8ddf96b7190f7414ad8965ccbe54bb6b384eaf
Naga Vadlapudi <NagaSnigdhesh.Vadlapudi1@T-Mobile.com>
2019-06-27 15:16:31 -0400

    initial commit

---
+++ src/components/MainContent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+function MainContent(){
+    return (
+        <main className="todo-list">
+          <TodoItem/>
+          <TodoItem/>
+          <TodoItem/>
+          <TodoItem/>
+        </main>
+    )
+}
+
+export default MainContent
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyInfo from './components/MyInfo'
+import './index.css';
+
+ReactDOM.render(<MyInfo/>,document.getElementById("root"));
