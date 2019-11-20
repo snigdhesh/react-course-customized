9b647d6870515867399592f18d2c0eea05c57aaf
Naga Vadlapudi <NagaSnigdhesh.Vadlapudi1@T-Mobile.com>
2019-11-20 11:39:35 -0500

    Added definition for 'capstone project'

--- src/capstone-project/CapstoneMainComponent.js
+++ src/capstone-project/CapstoneMainComponent.js
@@ -8,6 +8,9 @@ class CapstoneMainComponent extends Component{
         return(
           <div className="capstone-body">
             <CapstoneHeaderComponent/>
+            <p>What is a capstone project? </p>
+            <p>Source: Internet</p>
+            <p>A capstone project is a multifaceted assignment that serves as a culminating academic and intellectual experience for students, typically during their final year of high school or middle school, or at the end of an academic program or learning-pathway experience.</p>
             <CapstoneMemeGeneratorComponent/>
           </div>
         )
